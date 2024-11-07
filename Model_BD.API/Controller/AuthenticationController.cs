d84148221232b7f2668db71f6dba08914c25e48b
deepakraj-18 <dipakraaj18@gmail.com>
2024-11-07 22:47:21 +0530

    Project Creation

---
+++ Model_BD.API/Controller/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Model_BD.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+    }
+}
---
+++ Model_BD.API/Controller/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Model_BD.API.Controller
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+        public long LongInUserId
+        {
+            get
+            {
+                try
+                {
+                    string strValue = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+                    return string.IsNullOrEmpty(strValue) ? 0 : Convert.ToInt64(strValue);
+                }
+                catch (Exception)
+                {
+
+                    return 0;
+                }
+            }
+        }
+    }
+}
