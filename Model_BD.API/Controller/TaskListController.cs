8e4a6d9faa33915a03b254d82376690805009a12
deepakraj-18 <dipakraaj18@gmail.com>
2024-11-07 23:05:16 +0530

    Task List Implementation

---
+++ Model_BD.API/Controller/TaskListController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model_BD.BAL.IService;
+
+namespace Model_BD.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskListController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+        public TaskListController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+        public ActionResult Add()
+        {
+            var result=_taskService
+        }
+    }
+}
